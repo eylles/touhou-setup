6d2295d49b0aca7e7346422ca03e4282e6a68aba
eylles <ed.ylles1997@gmail.com>
2019-12-04 21:35:48 -0600

    minor fix

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ install tuhustp "$HOME"/.local/bin/tuhustp
 grep -qxF "PATH='$HOME/.local/bin:${PATH}'" "$HOME"/.bashrc || echo "PATH='$HOME/.local/bin:${PATH}'" >> "$HOME"/.bashrc
 grep -qxF "export PATH" "$HOME"/.bashrc || echo 'export PATH' >> "$HOME"/.bashrc
 # reload .bashrc
-source ~/.bashrc
+source "$HOME"/.bashrc
 
